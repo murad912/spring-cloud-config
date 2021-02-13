add0e6c988ebc37eb158d0e47ac2d922e1087b9e
murad <murad9@users.noreply.github.com>
2021-02-12 23:14:29 -0600

    first commit

---
+++ spring-cloud-config/src/main/java/com/springcloudconfigurationservice/SpringCloudConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springcloudconfigurationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigurationServiceApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-config/src/test/java/com/springcloudconfigurationservice/SpringCloudConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudconfigurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigurationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
